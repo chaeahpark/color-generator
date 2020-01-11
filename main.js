d76beb23186b9586eb3ff93565e3903df5f8c4bf
CPark <parkchaeah331@gmail.com>
2020-01-11 16:47:50 +0200

    main.js has been edited

--- main.js
+++ main.js
@@ -1,29 +1,71 @@
 class ColorGenerator {
   constructor() {
-    this.HexaCode = [];
-    this.bgColor;
-    this.displayColor;
+    this.hexaCode;
   }
 
   // When the user hit the generate btn
-  getHexaCode() {
+  randomHexaCode() {
     return (
-      "#" +
-      Math.random()
-        .toString(16)
-        .slice(2, 8)
+      (this.hexaCode =
+        "#" +
+        Math.random()
+          .toString(16)
+          .slice(2, 8)) + "9f"
     );
   }
 
-  changeColors() {
-    setInterval(this.getHexaCode, 3000);
-  }
-
-  stopDisplay() {
-    clearInterval(this.changeColor);
-  }
-
   searchColor(input) {
-    this.HexaCode = input;
+    this.hexaCode = input;
   }
 }
+
+const myPalette = new ColorGenerator();
+const changeColors = setInterval(showRandomColors, 3000);
+const bgColors = document.getElementsByClassName("bgColor");
+const generateBtn = document.getElementById("generate-btn");
+const stopBtn = document.getElementById("stop-btn");
+const searchBar = document.getElementById("search-bar");
+let userInput = "#";
+
+function showRandomColors() {
+  for (let i = 0; i < bgColors.length; i++) {
+    let hexaCode = myPalette.randomHexaCode();
+    bgColors[i].style.backgroundColor = hexaCode;
+    bgColors[i].children[0].textContent = hexaCode;
+  }
+}
+
+//As soon as a user opens the color generator.
+document.addEventListener("DOMContentLoaded", function() {
+  showRandomColors();
+  changeColors;
+});
+
+// When a user clicked the GENERATE button, show random hexa code and its color.
+generateBtn.addEventListener("click", function(e) {
+  e.preventDefault();
+  changeColors;
+});
+
+// When a user clicked the STOP button, stop generating random colors.
+stopBtn.addEventListener("click", function(e) {
+  e.preventDefault();
+  clearInterval(changeColors);
+});
+
+//Taking a user input and show the result
+searchBar.addEventListener("keydown", function(e) {
+  e.preventDefault();
+  clearInterval(changeColors);
+  console.log(e);
+
+  if (e.key === "Backspace") {
+    userInput = userInput.concat("");
+    searchBar.value = searchBar.value.substring(0, searchBar.value.length - 1);
+  } else {
+    userInput = userInput.concat(e.key);
+    searchBar.value = userInput;
+  }
+  //searchBar.setAttribute("value", e.key);
+  //console.log(e.key);
+});
