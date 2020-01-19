dc9a9654583c883e628eaff99b033f94f26f5841
CPark <parkchaeah331@gmail.com>
2020-01-19 20:40:27 +0200

    several things has been added: alert box, focus on search box, etc

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const copyBtn = document.querySelectorAll("copy-btn");
 const searchBar = document.getElementById("search-bar");
 const firstColorBox = document.getElementById("start");
 const colorDisplay = document.getElementById("colors");
+//variables for the timer
+let isTimerRunning = false;
+let timerID = 0;
+//Searchbar focus event indicator
+let searchBarFocused;
 
 //Generate hexa code randomly
 function randomHexaCode() {
@@ -28,9 +33,8 @@ function showRandomColors() {
   console.log("random");
   return randomColors;
 }
-//As soon as a user opens the color generator.
-var isTimerRunning = false;
-var timerID = 0;
+
+//Set the setInterval to change colors.
 function setColorTimer() {
   if (!isTimerRunning) {
     timerID = setInterval(showRandomColors, 1000);
@@ -39,8 +43,7 @@ function setColorTimer() {
   }
 }
 
-setColorTimer();
-
+//Remove the setInterval to stop generating colors.
 function clearColorTimer() {
   if (isTimerRunning) {
     clearInterval(timerID);
@@ -51,22 +54,74 @@ function clearColorTimer() {
 
 // Change the color display when the user uses the search bar.
 function searchModeDisplay() {
-  let colorCodeInput = document
-    .getElementById("search-bar")
-    .value.toUpperCase();
+  console.log(bgColors);
 
-  firstColorBox.style.backgroundColor = colorCodeInput;
-  firstColorBox.style.borderBottomLeftRadius = "0.5rem";
-  firstColorBox.style.borderBottomRightRadius = "0.5rem";
-  firstColorBox.children[0].children[0].textContent = colorCodeInput;
-
-  for (let i = 1; i < bgColors.length; i++) {
-    bgColors[i].style.backgroundColor = "white";
-    bgColors[i].children[0].children[0].textContent = "";
+  [...bgColors].forEach(bgColor => {
+    bgColor.style.backgroundColor = "white";
+    bgColor.children[0].children[0].textContent = "";
     //modify the following part.
-    bgColors[i].children[0].children[1].style.backgroundColor = "white";
-    bgColors[i].children[0].children[1].style.color = "white";
+    bgColor.children[0].children[1].style.backgroundColor = "white";
+    bgColor.children[0].children[1].style.color = "white";
+  });
+}
+
+// Show the HEX color when a user searches color through the searchbar
+function showSearchingColor() {
+  let colorCodeInput = document.getElementById("search-bar").value;
+
+  if (colorCodeInput.search(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/gi) >= 0) {
+    firstColorBox.style.backgroundColor = colorCodeInput;
+    firstColorBox.style.borderBottomLeftRadius = "0.5rem";
+    firstColorBox.style.borderBottomRightRadius = "0.5rem";
+    firstColorBox.children[0].children[0].textContent = colorCodeInput;
+    firstColorBox.children[0].children[1].style.backgroundColor = "#f2f0f0";
+    firstColorBox.children[0].children[1].style.color = "black";
   }
+}
+
+function verifyHexaCodeInput(input) {
+  let userInputCode = input.target.value;
+  const alertBox = document.createElement("div");
+  alertBox.id = "alertBox";
+  alertBox.style.backgroundColor = "#e34a20";
+  alertBox.style.color = "white";
+  alertBox.style.padding = "0.8rem 2rem";
+  alertBox.style.height = "3rem";
+  alertBox.style.fontSize = "1.2 rem";
+  alertBox.style.borderRadius = "0.3rem";
+  const alertText = document.createTextNode("Please type in valid inputs.");
+  alertBox.appendChild(alertText);
+
+  if (userInputCode.search(/[G-Zg-z]/i) >= 0) {
+    colorDisplay.insertBefore(alertBox, firstColorBox);
+    alertBoxCreated = document.getElementById("alertBox");
+
+    setTimeout(function() {
+      colorDisplay.removeChild(alertBoxCreated);
+    }, 2000);
+  }
+}
+
+// copy hexa code.
+function copyColorCode() {
+  const copyBtns = document.querySelectorAll(".copy-btn");
+
+  [...copyBtns].forEach(copyBtn => {
+    copyBtn.addEventListener("click", function() {
+      let copiedColorCode = copyBtn.previousElementSibling;
+      let text = copiedColorCode.textContent;
+      const input = document.createElement("input");
+
+      document.body.appendChild(input);
+      input.style = "position: absolute; left: -100px; top: -100px";
+      input.value = text;
+      input.focus();
+      input.select();
+      document.execCommand("copy");
+
+      document.body.removeChild(input);
+    });
+  });
 }
 
 //Recover color boxes when the user clicks the generate button after using the search bar.
@@ -83,30 +138,9 @@ function recoverColorBoxes() {
   });
 }
 
-// copy hexa code.
-function copyColorCode() {
-  const copyBtns = document.querySelectorAll(".copy-btn");
-
-  [...copyBtns].forEach(copyBtn => {
-    copyBtn.addEventListener("click", function(e) {
-      let copiedColorCode = copyBtn.previousElementSibling;
-      let text = copiedColorCode.textContent;
-      const input = document.createElement("input");
-
-      document.body.appendChild(input);
-      input.style = "position: absolute; left: -100px; top: -100px";
-      input.value = text;
-      input.focus();
-      input.select();
-      document.execCommand("copy");
-      document.body.removeChild(input);
-    });
-  });
-}
-
 // When the document is first loaded
 document.addEventListener("DOMContentLoaded", function() {
-  console.log(bgColors);
+  searchBarFocused = false;
   showRandomColors();
   setColorTimer();
   //Copy button is enabled
@@ -116,7 +150,9 @@ document.addEventListener("DOMContentLoaded", function() {
 // When a user clicked the GENERATE button, show random hexa code and its color.
 generateBtn.addEventListener("click", function(e) {
   e.preventDefault();
+
   recoverColorBoxes();
+  showRandomColors();
   setColorTimer();
 });
 
@@ -128,21 +164,41 @@ stopBtn.addEventListener("click", function(e) {
 
 // When the search bar get focused, stop generating random colors.
 searchBar.addEventListener("focus", function() {
+  searchBarFocused = true;
   clearColorTimer();
+  searchModeDisplay();
+  console.log(searchBarFocused);
+});
+
+searchBar.addEventListener("focusout", function() {
+  searchBarFocused = false;
+  recoverColorBoxes();
+  showRandomColors();
+  setColorTimer();
+  console.log(searchBarFocused);
 });
 
 //When a user type in the hexa code color, change the output display.
 searchBar.addEventListener("keyup", function(e) {
   e.preventDefault();
-  searchModeDisplay();
+  searchBarFocused = true;
+  showSearchingColor();
+  verifyHexaCodeInput(e);
 });
 
 colorDisplay.addEventListener("mouseover", function() {
+  console.log(searchBarFocused);
+
   clearColorTimer();
-  console.log("over");
 });
 
 colorDisplay.addEventListener("mouseleave", function() {
-  setColorTimer();
-  console.log("leave");
+  console.log(searchBarFocused);
+
+  if (searchBarFocused === true) {
+    searchModeDisplay();
+  }
+  if (searchBarFocused === false) {
+    setColorTimer();
+  }
 });
